acba93423c1a7e8a54937b6bb431fca09b0cd010
vadim <gabderahmanov99@gmail.com>
2022-10-10 22:12:39 +0300

    zero field compression

--- app/src/main/java/com/example/mathmethodssolving/Starter.java
+++ app/src/main/java/com/example/mathmethodssolving/Starter.java
@@ -1,19 +1,17 @@
 package com.example.mathmethodssolving;
 
 
-import java.util.Arrays;
-
 public class Starter {
     public static void main(String[] args) {
         int[][] a = {
                 {2, 10, -3, 38},
                 {-3, -24, 5, -86},
-                {1, -3, -5, 27}
+                {1, 3, -5, 27}
         };
 
         int[][] b = GaussMethod.pivotSwap(a,0);
+        GaussMethod.print(b);
         GaussMethod.zeroField(b);
-        //nullable
         GaussMethod.print(b);
     }
 }
